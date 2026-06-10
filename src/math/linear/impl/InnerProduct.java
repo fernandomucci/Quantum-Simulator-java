4a04f83565ced434ecc5ee6a4455aacd044a7b16
FernandoMucci <fer.sena.mucci@gmail.com>
2026-06-10 16:07:21 -0300

    feat: add norm calculation

--- src/math/linear/impl/InnerProduct.java
+++ src/math/linear/impl/InnerProduct.java
@@ -8,12 +8,29 @@ public class InnerProduct
     {
         if (m1 == null || m2 == null)
         {
-            throw new IllegalArgumentException("the matrices cannot be null.");
+            throw new IllegalArgumentException("the matrice cannot be null.");
         }
 
         //Tr(A† * B)
         ComplexNumber result = m1.dagger().matrixMultiplication(m2).trace();
 
         return result;
+    }
+
+    public static ComplexNumber normValue(ComplexMatrices m1)
+    {
+        if(m1 == null)
+        {
+            throw new IllegalArgumentException("the matrice cannot be null.");   
+        }
+
+        //< v , v >
+        ComplexNumber innerProductByItself = innerPeodutcValue(m1, m1);
+
+        //square < v , v >
+        double normDouble = Math.sqrt(innerProductByItself.getReal());
+
+        return new ComplexNumber(normDouble, 0);
+       
     }
 }
