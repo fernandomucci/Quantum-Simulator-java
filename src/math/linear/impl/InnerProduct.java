d13b9674677845a7cc8add620fd5aee0a5bfa5dc
FernandoMucci <fer.sena.mucci@gmail.com>
2026-07-08 23:17:43 -0300

    feat: implement distance metric for complex matrices

--- src/math/linear/impl/InnerProduct.java
+++ src/math/linear/impl/InnerProduct.java
@@ -4,7 +4,7 @@ import math.core.ComplexNumber;
 
 public class InnerProduct
 {
-    public static ComplexNumber innerProdutcValue(ComplexMatrices m1,ComplexMatrices m2)
+    public static ComplexNumber innerProductValue(ComplexMatrices m1,ComplexMatrices m2)
     {
         if (m1 == null || m2 == null)
         {
@@ -25,13 +25,26 @@ public class InnerProduct
         }
 
         //< v , v >
-        ComplexNumber innerProductByItself = innerProdutcValue(m1, m1);
+        ComplexNumber innerProductByItself = innerProductValue(m1, m1);
 
         //square < v , v >
         double normDouble = Math.sqrt(innerProductByItself.getReal());
 
         return new ComplexNumber(normDouble, 0);
        
+    }
+
+    public static ComplexNumber distance(ComplexMatrices m1, ComplexMatrices m2)
+    {
+        if(m1 == null || m2 == null)
+        {
+            throw new IllegalArgumentException("the matrice cannot be null.");   
+        }
+
+        ComplexMatrices matrixSub = m1.subtract(m2);
+
+        return normValue(matrixSub);
+
     }
 
 }
